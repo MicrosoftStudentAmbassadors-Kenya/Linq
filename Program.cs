80f9d8b229b66553c40544b939885dc8a5267cc4
Koima-Sam <56549814+Koima-Sam@users.noreply.github.com>
2019-10-30 15:20:47 +0300

    Add files via upload
    My repo uploaded on Wednesday

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ingridient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
