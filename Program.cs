9c84158c0ea423ac7803eff5a2be7312b8b25552
Izoh06 <46515018+Izoh06@users.noreply.github.com>
2019-10-30 05:49:47 +0300

    Add files via upload
    Added! Wow

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Ingredients
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ingredient[] ingredients =
+            {
+            new Ingredient{Name = "Sugar", Calories=500},
+            new Ingredient{Name = "Egg", Calories=100},
+            new Ingredient{Name = "Milk", Calories=150},
+            new Ingredient{Name = "Flour", Calories=50},
+            new Ingredient{Name = "Butter", Calories=200}
+            };
+            Console.Read();
+            
+            foreach (var item in ingredients)
+               {
+                 System.Console.WriteLine($"name:{item.Name}");
+               }
+                  
+        }
+    }
+}
